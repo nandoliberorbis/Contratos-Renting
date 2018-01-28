0675ed70030f2ebc0328c7c05c4c808619f31692
Fernando Sánchez <nando@liberorbis.com>
2018-01-28 13:01:02 -0500

    Actualizacion JS para traer texto de anexos de contrato

--- contratos/contratos/doctype/anexo_contrato_marco/anexo_contrato_marco.js
+++ contratos/contratos/doctype/anexo_contrato_marco/anexo_contrato_marco.js
@@ -4,5 +4,18 @@
 frappe.ui.form.on('Anexo Contrato Marco', {
 	refresh: function(frm) {
 
+	},
+        planti_acm: function(frm) {
+		frappe.call({
+			"method": "frappe.client.get",
+			args: {
+				doctype: "Texto Anexos Contrato Marco",
+				name: frm.doc.planti_acm
+			},
+			callback: function(data) {
+				frappe.model.set_value(frm.doctype,
+				frm.docname,"txt_acm",data.message.texto_ane_contrato)
+			}
+		})
 	}
 });
